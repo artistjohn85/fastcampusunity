5872c645f0ee8231def09ccc89362060b20c71ac
artistjohn85 <win1042@gmail.com>
2024-01-01 21:26:48 +0900

    UGUI Layout

--- Assets/Scripts/CameraResolution.cs
+++ Assets/Scripts/CameraResolution.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Resolution : MonoBehaviour
+public class CameraResolution : MonoBehaviour
 {
     void Update() 
     { 
